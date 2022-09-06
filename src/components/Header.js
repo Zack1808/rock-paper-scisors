b03329d8f60443291a3a8f900a74c3cc470afa22
Jean-Pierre Novak <jeanpierrenovak21@gmail.com>
2022-09-06 22:15:26 +0200

    Styled the Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,24 @@
 import React from 'react';
 
+// Importing the css file
+import "../css/Header.css";
+
 // Creating the Header component
 const Header = ({ score }) => {
     return (
-        <div className='header'>Score: {score}</div>
+        <div className='header'>
+            <div className="text">
+                <span>Rock</span>
+                <span>Paper</span>
+                <span>Scissors</span>
+            </div>
+            <div className="score-box">
+                <span>Score: </span>
+                <div className="score-box__score">
+                    {score}
+                </div>
+            </div>
+        </div>
     )
 };
 

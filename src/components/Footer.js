3b6e7304247a24ee87b2a1259b57af0db4808479
Jean-Pierre Novak <jeanpierrenovak21@gmail.com>
2022-09-08 08:29:04 +0200

    Added syling to the Footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,20 @@
 import React from 'react';
 
+// Importing the style file
+import "../css/Footer.css";
+
 // Creating the Footer component
 const Footer = () =>{
     return (
-        <div>Footer</div>
+        <footer className="footer">
+                <div className='attribution'>
+                Challanged by <a href='https://www.frontendmentor.io?ref=challange' target="_blank">Frontend Mentor</a>
+                Code by JPN
+            </div>
+            <button className="rules">
+                Rules
+            </button>
+        </footer>
     )
 };
 

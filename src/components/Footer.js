7f0c27da97ff0b344fda221f417c7617a9ea1bbc
Jean-Pierre Novak <jeanpierrenovak21@gmail.com>
2022-09-08 08:55:08 +0200

    Styled the Modal component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,20 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+// Importing the costume made components
+import Modal from './Modal';
 
 // Importing the style file
 import "../css/Footer.css";
 
 // Creating the Footer component
 const Footer = () =>{
+
+    const [modal, setModal] = useState(false);
+
+    const toggleModal = () =>{
+        setModal(!modal)
+    }
+
     return (
-        <footer className="footer">
+        <>
+            <footer className="footer">
                 <div className='attribution'>
-                Challanged by <a href='https://www.frontendmentor.io?ref=challange' target="_blank">Frontend Mentor</a>
-                Code by JPN
-            </div>
-            <button className="rules">
-                Rules
-            </button>
-        </footer>
+                    Challanged by <a href='https://www.frontendmentor.io?ref=challange' target="_blank">Frontend Mentor</a>
+                    Code by JPN
+                </div>
+                <button className="rules" onClick={toggleModal}>
+                    Rules
+                </button>
+            </footer>
+            {
+                modal ? <Modal toggle={toggleModal} /> : null
+            }
+        </>
     )
 };
 
--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import ReactDOM from 'react-dom'
+
+// importing images
+import close from '../images/icon-close.svg';
+import rules from '../images/image-rules.svg'
+
+// Importing the styling
+import '../css/Modal.css';
 
 // Creating the Modal component
-const Modal = () => {
-    return(
-        <div>Modal</div>
+const Modal = ({ toggle }) => {
+    return ReactDOM.createPortal(
+        <div className="modal-container">
+            <div className="modal-box">
+                <div className="modal-header">
+                    <h1>Rules</h1>
+                    <button onClick={toggle}>
+                        <img src={close} alt="Close" />
+                    </button>
+                </div>
+                <img src={rules} alt="Rules" />
+            </div>
+        </div>, 
+        document.getElementById('modal')
     )
 };
 

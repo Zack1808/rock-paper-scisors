6087d82a838fc9ff99d5adfd5269084cd2b61d94
Jean-Pierre Novak <jeanpierrenovak21@gmail.com>
2023-02-04 14:49:51 +0100

    Added favicon and created new App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,37 +1,9 @@
-import React, { useState } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
-// Importing the costume made components
-import Header from './Header';
-import Game from './Game';
-import GameBoard from './GameBoard';
-import Modal from './Modal'
-import Footer from './Footer';
-
-// Importing the style file
-import '../css/App.css';
+import React from "react";
 
 // Creating the App component
 const App = () => {
-
-    const [playerChoice, setPlayerChoice] = useState("");
-    const [score, setScore] = useState(0);
-
-    return (
-        <BrowserRouter basename={process.env.PUBLIC_URL}>
-            <div className="rotate">
-                <h1>Please rotate the screen to portrait mode to play</h1>
-            </div>
-            <div className='container'>
-                <Header score={score} />
-                <Routes>
-                    <Route exact path='/' element={<Game setPlayerChoice={setPlayerChoice} />} />
-                    <Route exact path='/game' element={<GameBoard playerChoice={playerChoice} score={score} setScore={setScore} />} />
-                </Routes>
-            </div>
-            <Footer />
-        </BrowserRouter>
-    )
+  return <div className="app-container"></div>;
 };
 
+// Exporting the App component
 export default App;
